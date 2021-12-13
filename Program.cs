0e9082e144cc426594d4387bc41f9205a4d22d9e
kaluza_marek <kaluza@adash.cz>
2021-12-13 17:01:53 +0100

    day 11

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace aoc2021
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("{0, 8}",Day13.Task2());
+            //Console.WriteLine("{0, 8}",Day13.Task2());
+            Console.WriteLine("{0}", Day11.Task2());
             Console.ReadKey();
         }
     }
