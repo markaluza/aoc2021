d5a8c77af18c31943ddd08d437124615deb0a576
kaluza_marek <kaluza@adash.cz>
2021-12-13 17:01:44 +0100

    day 11

---
+++ day11.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace aoc2021
+{
+    class Day11
+    {
+        
+        private static int [,] GetInput()
+        {
+            var lines = aocIO.GetStringList("day11.txt");
+            var map = new int [lines[0].Length, lines.Count];
+
+            for (int y =0; y < lines.Count; y++)
+            {
+                for (int x= 0; x < lines[y].Length; x++)
+                    map[x,y] = lines[y][x] - '0';
+            }
+            return map;
+        }
+
+        private static void Flash(ref int [,] map, int x, int y, ref HashSet<(int, int)> flashed)
+        {
+
+            if (x < 0 || x >= map.GetLength(0) ||
+                y < 0 || y >= map.GetLength(1)) return;
+
+            map[x,y]++;
+
+            if (map[x,y] >9)
+            {
+                if (flashed.Contains((x,y))) return;
+                flashed.Add((x,y));
+
+                Flash(ref map, x-1, y-1, ref flashed);
+                Flash(ref map, x, y-1, ref flashed);
+                Flash(ref map, x+1, y-1, ref flashed);
+                Flash(ref map, x+1, y, ref flashed);
+                Flash(ref map, x+1, y+1, ref flashed);
+                Flash(ref map, x, y+1, ref flashed);
+                Flash(ref map, x-1, y+1, ref flashed);
+                Flash(ref map, x-1, y, ref flashed);
+
+            };
+        }
+        private static int Step(ref int [,] map)
+        {
+            var flashes = new HashSet<(int, int)>();
+            for (int y =0; y < map.GetLength(0); y++)
+            {
+                for (int x =0; x < map.GetLength(1); x++)
+                {
+                   Flash(ref map, x,y, ref flashes);
+                }
+            }
+
+            for (int y =0; y < map.GetLength(0); y++)
+            {
+                for (int x =0; x < map.GetLength(1); x++)
+                {
+                   if (map[x,y] > 9) map[x,y] = 0;
+                }
+            }
+
+            return flashes.Count;
+        }
+
+       public static long Task1()
+       {
+            var map = GetInput();
+
+            long flashes = 0;
+            for (int i =0; i < 100; i++)
+            {
+                flashes+= Step(ref map);
+            }
+
+            return flashes;
+       }   
+
+       public static long Task2()
+       {                
+            var map = GetInput();
+
+            for (long i =1; ; i++)
+            {
+                if (Step(ref map) == 100) return i;
+            }
+            
+            return 0;
+       }       
+    }
+}
