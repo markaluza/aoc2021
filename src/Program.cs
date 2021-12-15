e9d1dbc9632102e45c5131b7bd0f5fb164377c23
markaluza <markaluza@seznam.cz>
2021-12-15 21:27:18 +0100

    day 15

--- src/Program.cs
+++ src/Program.cs
@@ -6,7 +6,7 @@ namespace aoc2021
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("{0}", Day15.Task1());
+            Console.WriteLine("{0}", Day15.Task2());
             Console.ReadKey();
         }
     }
